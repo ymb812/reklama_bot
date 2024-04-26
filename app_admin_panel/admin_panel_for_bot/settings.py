d67f0cbe48710ca66f40fe6242b1af829da1e5c3
Kotov Ilya <the3k27@gmail.com>
2024-04-27 01:13:14 +0300

    Edit allowed hosts

--- app_admin_panel/admin_panel_for_bot/settings.py
+++ app_admin_panel/admin_panel_for_bot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = settings.secret_key.get_secret_value()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = settings.debug_mode
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'host.docker.internal', '5.63.153.156']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'host.docker.internal', '185.43.4.189']
 
 # Application definition
 
